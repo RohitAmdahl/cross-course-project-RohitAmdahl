32a954aeddb9c118bb3356831fea3eac194a5829
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-02 14:58:48 +0200

    iupdate

--- js/cart.js
+++ js/cart.js
@@ -9,7 +9,66 @@ console.log(priceOFitems);
 const products = document.querySelectorAll(".cards")
 console.log(products);
 
+ const ShoppingCartHTML= function() {
+   
+  if (productInCart.length > 0) {
+
+    
+  }
+  else{
+    document.querySelector.(".checkout").classList.add("hidden");
+    cartItems.innerHTML = `<p class="empty">Your shopping cart is empty</p> `;
+  
+  
+  }
+
+
+ }
+
+
+
+function updateProductInCart(product) {
+  for (let i = 0; i < productInCart.length; i++) {
+    if (productInCart[i].id === data-id) {
+      productInCart[i].count += 1;
+      productInCart[i].price = productInCart[i].baseCost * productInCart[i].count;
+      return;
+    }
+    
+    
+  }
+  
+  productInCart.push(product)
+}
+
+
 
 products.forEach(function (products) {
-  console.log(products);
-})
+  
+  products.addEventListener("click", (card) => {
+    if (card.target.classList.contains("addtocart")) {
+
+      const dataID = card.target.dataset.dataID;
+      const productName = card.querySelector(".productName").innerHTML;
+      console.log(productName);
+      const productPrice = card.querySelector(".cost").innerHTML;
+      const productImage = card.querySelector("img").src;
+
+      let productCart ={
+        name : productName,
+        Image: productImage,
+        id : dataID,
+        count : 1,
+        price : +productPrice,
+        baseCost : +productPrice
+      } 
+
+      updateProductInCart(productInCart);
+      ShoppingCartHTML();
+    }
+
+
+  });
+
+
+});
