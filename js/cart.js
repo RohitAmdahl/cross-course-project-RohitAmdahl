548309530b60f2b7f3a11704219fee843b4ec208
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-02 13:44:16 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -6,3 +6,10 @@ console.log(cartItems);
 const priceOFitems = document.querySelector(".total-price");
 console.log(priceOFitems);
 
+const products = document.querySelectorAll(".cards")
+console.log(products);
+
+
+products.forEach(function (products) {
+  console.log(products);
+})
