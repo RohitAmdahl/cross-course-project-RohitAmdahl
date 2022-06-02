6410486af2611d2b771898e94972c64d5fec7f55
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-02 22:26:53 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -1,13 +1,35 @@
+// import { products } from "/js/product.js";
 
+// const productCards = document.querySelector(".cards");
 
+// products.forEach(function(product){
+
+//   productCards.innerHTML += 
+//   `
+//   <div class="cards">
+//   <div class="products">
+//       <img src="${products.image}" alt="${products.description}">
+//   </div>
+//   <div class="price-1">
+//    <p class="productName">${products.name}</p>
+//    <p class="cost">${products.price}</p>
+//   </div>
+//   <div class="add-to-cart-btn" >
+//    <button class="btn-small">Add to Cart</button>
+//  </div>
+// </div>
+
+//   `
+
+// }) 
 
 let productCart = [];
 
  const button = document.querySelectorAll(".btn-small")
  console.log(button);
 
- const shoppingCart = document.querySelector("#cart");
-console.log(shoppingCart);
+//  const shoppingCart = document.querySelector("#cart");
+// console.log(shoppingCart);
 
  const products = document.querySelectorAll(".cards")
  console.log(products);
@@ -15,9 +37,9 @@ console.log(shoppingCart);
 
 button.forEach(function (button) {
   button.onclick = function(event) {
-    //console.log(event.target.dataset);
+    console.log(event.target.dataset);
     productCart.push(event.target.dataset.products);
-    const itemsToAddTocart = productCart.find(products => products.id ===event.target.dataset.products);
+    const itemsToAddTocart = productCart.find(products => card.id === event.target.dataset.products);
     console.log(itemsToAddTocart);
 
   }
---
+++ js/product.js
@@ -0,0 +1,187 @@
+  
+  
+  export const products = [
+
+  {
+  name: "Green jacket",
+  description: "rainy day jacket water proof",
+  image: "/images/hiking-2.png",
+  id: "1",
+  price: "1950kr,-",
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "/images/manacketgreen.png",
+    id: "2",
+    price: "1950kr,-",
+  
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+
+    id: "3",
+    price: "1950kr,-",
+
+  }, 
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/hiking-1.png",
+    id: "4",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/manjacketpink.png",
+    id: "5",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/manjacketpink.png",
+    id: "6",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/hiking.png",
+    id: "7",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/hiking-1.png",
+    id: "8",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day green jacket water proof",
+    image: "/images/womenunisexbluelight.png",
+    id: "9",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "/images/hiking-1.png",
+    id: "10",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "/images/hiking.png",
+    id: "11",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/hiking-2.png",
+    id: "12",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/womenunisexbluelight.png",
+    id: "13",
+    price: "1950kr,-",
+    
+  },
+
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/manacketgreen.png",
+    id: "14",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/hiking-2.png",
+    id: "15",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/hiking.png",
+    id: "16",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/hiking-2.png",
+    id: "17",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/womenunisexbluelight.png",
+    id: "18",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/hiking.png",
+    id: "19",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/womenunisexbluelight.png",
+    id: "20",
+    price: "1950kr,-",
+    
+  },
+  {
+    name: "Green jacket",
+    description: "rainy day jacket water proof",
+    image: "images/womenunisexbluelight.png",
+    id: "21",
+    price: "1950kr,-",
+    
+  },
+
+
+]
+
