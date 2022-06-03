9082246c659caab37d2a41f17b1f54da91dc1318
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-03 13:08:20 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -2,11 +2,9 @@
 import { productsArray } from "/js/product.js"
 console.log(productsArray);
 
-//  const button = document.querySelectorAll(".btn-small")
-//  console.log(button);
 
-//   const shoppingCart = document.querySelector("#cart");
-//   console.log(shoppingCart);
+
+
 
 
 const itemsWrapper= document.querySelector(".women-section");
@@ -19,31 +17,34 @@ const itemsWrapper= document.querySelector(".women-section");
       <div class="products">
       <img src="${product.image}" alt="" class="products">
       </div>
-      <div class="price_1">
+      <div class="price-1">
       <p class="productName">${product.name}</p>
       <p class="cost">${product.price}</p>
       </div>
-      <div class="add_to_cart_btn">
+      <div class="add-to-cart-btn">
       <button class="btn-small">Add to cart</button>
       </div>
  
-    
       </div>
    
 `
  })
 
 
-// let productCart = [];
 
 
-// button.forEach(function (button) {
-//   button.onclick = function(event) {
-//     console.log(event.target.dataset);
-//     productCart.push(event.target.dataset.products);
-//     const itemsToAddToCart = productCart.find(products => products.id === event.target.dataset.products);
-//     console.log(itemsToAddToCart);
-//     openCart();
+
+
+//let productCart = [];
+
+
+//  button.forEach(function (button) {
+//    button.onclick = function(event) {
+//      console.log(event.target.dataset);
+//      productCart.push(event.target.dataset.products);
+//      const itemsToAddToCart = productCart.find(products => products.id === event.target.dataset.products);
+//      console.log(itemsToAddToCart);
+//      openCart();
 
 //   }
 //   function openCart(cartItems){
