a9d4537cb4d22ecaa8c088130e3e9c33ff8e0b23
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-03 17:17:31 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -35,37 +35,35 @@ let productCartArray = [];
  const button = document.querySelectorAll(".add-to-cart-btn")
  console.log(button);
 
- button.onclick
+ 
 
 button.forEach(function(button) {
    button.onclick = function(event) {
-      //console.log(event.target.dataset.product);
-      productCartArray.push(event.target.dataset.product);
-      const cartProducts = productsArray.find(itemsWrapper => itemsWrapper.id === event.target.dataset.product)
-      console.log(productCartArray);
+      console.log(event.target.dataset.product);
+       productCartArray.push(event.target.dataset.product);
+       const cartProducts = productsArray.find(itemsWrapper => itemsWrapper.id === event.target.dataset.product)
+       console.log(productCartArray);
       productCartArray.push(cartProducts);
-     
+     window.location.href = "/products/products.html"
    }
 })
 
 
-button.onclick = function toDetailsPage() {
-   window.location.href = '/products/products.html'
-}
-
    function showCartPage(cartItems) {
-
-      cartItems.foreach(function() {
-      price.innerHTML += `<h1 class="menswear">Unisex</h1>
-                          <h2 class="sub-heading">Universal design winter jackets and water resistant.</h2>
-                            <div class="price-2">
-                              <p>${product.name}</p>
-                              <p>${product.price}</p>
-                              </div>
-                              <div class="price-2">
-                              <p>${product.Total}</p>
-                              <p>${product.price}</p>
-                              
+      cartIcon.style.display="block"
+      cartItems.foreach(function(cartElement) {
+      price.innerHTML += `        <div class ="cards">
+                                    <div class="products">
+                                    <img src="${product.image}" alt="" class="products">
+                                    </div>
+                                    <div class="price-2">
+                                    <p class="productName">${product.name}</p>
+                                    <p class="cost">${product.price}</p>
+                                    </div>
+                                    <div class="add-to-cart-btn">
+                                    <button class="btn-small" data-product = ${product.id}>Add to cart</button>
+                                    </div>
+                                  </div>
                                `
    })
 
