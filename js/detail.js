a03b1f9865dffbad752af563290937acc87fdbc3
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-04 00:39:06 +0200

    update

--- js/detail.js
+++ js/detail.js
@@ -3,17 +3,12 @@
 import {productCartArray} from "/js/product.js";
 console.log(productCartArray);
 
+// const url =  product.html?id=123;
+
 const queryString = document.location.search;
-
 console.log(queryString);
-
 const params = new URLSearchParams(queryString);
-
-console.log(params)
-
+console.log(params);
 const id = params.get("id");
-
 console.log(id);
 
-const product = products.find(product => product.id === id)
-console.log(product);
