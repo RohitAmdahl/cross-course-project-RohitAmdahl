70902c2daf3412b7ae9c62cca0f1d0e61270d0bf
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-03 16:44:38 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -54,7 +54,7 @@ button.onclick = function toDetailsPage() {
 }
 
    function showCartPage(cartItems) {
-      
+
       cartItems.foreach(function() {
       price.innerHTML += `<h1 class="menswear">Unisex</h1>
                           <h2 class="sub-heading">Universal design winter jackets and water resistant.</h2>
@@ -72,5 +72,17 @@ button.onclick = function toDetailsPage() {
 
 }
 
+function myFunction() {
+   var number = document.querySelector("quantity-input").value;
+   var x = number + 1;
+   document.getElementById("demo").innerHTML = x;
+ }
 
+let x = 1950;
+let y = 1;
+let z = x * y;
+ 
 
+btn.addEventListener("click", function () {
+  
+});
