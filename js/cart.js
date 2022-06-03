0b1496288abad059a12baebfddd7086e583fada9
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-03 18:33:22 +0200

    update

--- js/cart.js
+++ js/cart.js
@@ -3,10 +3,11 @@ import { productsArray } from "/js/product.js"
 console.log(productsArray);
 
 const itemsWrapper= document.querySelector(".women-section");
-const cartIcon = document.querySelector("#cart");
-console.log(cartIcon);
-const price = document.querySelectorAll(".price-2");
-console.log(price);
+
+const cart = document.querySelector("#cart");
+const cartList = document.querySelector(".classlist");
+console.log(cart);
+console.log(cartList);
 
 
 
@@ -44,27 +45,21 @@ button.forEach(function(button) {
        const cartProducts = productsArray.find(itemsWrapper => itemsWrapper.id === event.target.dataset.product)
        console.log(productCartArray);
       productCartArray.push(cartProducts);
-     window.location.href = "/products/products.html"
+   //   window.location.href = "/products/products.html"
    }
 })
 
 
-   function showCartPage(cartItems) {
-      cartIcon.style.display="block"
+   function CartPage(cartItems) {
+      cart.style.display = "block";
+      cart.style.color = "red";
+      cartList.innerHTML= "";
       cartItems.foreach(function(cartElement) {
-      price.innerHTML += `        <div class ="cards">
-                                    <div class="products">
-                                    <img src="${product.image}" alt="" class="products">
-                                    </div>
-                                    <div class="price-2">
-                                    <p class="productName">${product.name}</p>
-                                    <p class="cost">${product.price}</p>
-                                    </div>
-                                    <div class="add-to-cart-btn">
-                                    <button class="btn-small" data-product = ${product.id}>Add to cart</button>
-                                    </div>
-                                  </div>
-                               `
+        cartList.innerHTML += `<div class="classlist">
+                             <p>${cartElement.name}<p>
+                             <img src="${cartElement.image}" alt="" class="products">
+                             </div>  `
+                          
    })
 
 
