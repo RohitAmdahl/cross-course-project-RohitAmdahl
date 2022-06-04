23b0f964dd4b127814294fee28cfe2ff4d58e56c
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-04 19:05:54 +0200

    update

--- js/detail.js
+++ js/detail.js
@@ -6,14 +6,20 @@ const details = document.querySelector("#datalist")
 console.log(details);
 const productInfo  = document.querySelector(".card_details")
 console.log(productInfo);
-
+ const counting = document.getElementById("numberCount")
+ console.log(counting);
 const plus = document.querySelector("#plus")
 console.log(plus);
+
 const minus = document.querySelector("#minus")
 console.log(minus);
 const cart = document.querySelector("#cart")
 console.log(cart);
 // const url =  product.html?id=123;
+
+
+
+
 
 const url = `http://127.0.0.1:5501/products/products.html?id=123`
 console.log(url);
@@ -24,4 +30,4 @@ console.log(params);
 const id = params.get("id");
 console.log(id);
 
-
+ 
