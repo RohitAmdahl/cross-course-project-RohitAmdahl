1af1d53a2104ccf65d265fa7c699c3a8fad858d2
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-06-05 00:51:26 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,11 +16,11 @@ let productCartArray = [];
  productsArray.forEach(function(product){
    itemsWrapper.innerHTML  +=
 
-   ` 
+   `  
       <div class ="cards">
          <div class="products">
-         
-         <a href="/products/products.html"><img src="${product.image}" alt="" class="products"></a>
+         <a href="/products/products.html">
+        <img src="${product.image}" alt="" class="products"></a> 
          </div>
          <div class="price-1">
          <p class="productName">${product.name}</p>
@@ -29,10 +29,12 @@ let productCartArray = [];
          <div class="add-to-cart-btn">
          <button class="btn-small" data-product = ${product.id}>Add to cart</button>
          </div>
-      </div>     
-   
+      </div> 
+         
 `
  })
+
+
 
  const button = document.querySelectorAll(".add-to-cart-btn")
  console.log(button);
