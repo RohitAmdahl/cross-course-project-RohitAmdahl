207396aeeba6e1420f38e2dc3ff57864c9de974b
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-08-21 01:28:10 +0200

    update

--- js/product.js
+++ js/product.js
@@ -1,3 +1,14 @@
+// const mainUrl = "https://www.rkamdahl.no/wp-json/wc/v3/products?consumer_key=ck_946e569b72881be9b01c9ebcbfb9761e36118143&consumer_secret=cs_cd563c29f6ce7fc749515dfe3912ec618804f8b6";
+
+
+const mainUrl = "https://www.rkamdahl.no/wp-json/wc/store/products";
+
+async function getProduct(url){
+  const response = await fetch(url);
+  const products =  await response.json();
+  console.log(products);
+}
+getProduct(mainUrl);
 /*
   export const productsArray = [
 
