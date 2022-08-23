8cf253ba1eb69e7625787dab9dcfb40d5cf4b8ac
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-08-23 16:26:55 +0200

    update

--- js/main.js
+++ js/main.js
@@ -2,64 +2,44 @@
 //import { productsArray } from "/js/product.js"
 // console.log(productsArray);
 
-const itemsWrapper= document.querySelector(".women-section");
-
-const cart = document.querySelector("#cart");
-const cartList = document.querySelector(".classlist");
-//console.log(cart);
-//console.log(cartList);
-
-// let productCartArray = [];
- productsArray.forEach(function(product){
-   itemsWrapper.innerHTML  +=
-
-   `  
-      <div class ="cards">
-         <div class="products">
-         <a href="/products/products.html">
-        <img src="${product.image}" alt="" class="products"></a> 
-         </div>
-         <div class="price-1">
-         <p class="productName">${product.name}</p>
-         <p class="cost">${product.price}</p>
-         </div>
-         <div class="add-to-cart-btn">
-         <button class="btn-small" data-product = ${product.id}>Add to cart</button>
-         </div>
-      </div> 
-         
-`
-  })
-
- const button = document.querySelectorAll(".add-to-cart-btn")
- console.log(button);
-
-button.forEach(function(button) {
-   button.onclick = function(event) {
-      console.log(event.target.dataset.product);
-       const cartProducts = productsArray.find(itemsWrapper => itemsWrapper.id === event.target.dataset.product)
-       productCartArray.push(cartProducts);
-       CartPage(productCartArray);
-       localStorage.setItem("cartlist", JSON.stringify(productCartArray));
-   }
-})
 
 
-   function CartPage(cartItems) {
-      cartList.style.display = "block";
-      cartList.style.color = "red";
-      cartList.innerHTML= "";
-      cartItems.forEach(function(cartElement) {
-        cartList.innerHTML += `
-                              <div class="classlist">
-                             <span>${cartElement.id}<span>
-                             <span>${cartElement.name}<span>
-                             </div>  
-                             `
+// const cart = document.querySelector("#cart");
+// const cartList = document.querySelector(".classlist");
+// //console.log(cart);
+// //console.log(cartList);
+
+// // let productCartArray = [];
+
+//  const button = document.querySelectorAll(".add-to-cart-btn")
+//  console.log(button);
+
+// button.forEach(function(button) {
+//    button.onclick = function(event) {
+//       console.log(event.target.dataset.product);
+//        const cartProducts = productsArray.find(itemsWrapper => itemsWrapper.id === event.target.dataset.product)
+//        productCartArray.push(cartProducts);
+//        CartPage(productCartArray);
+//        localStorage.setItem("cartlist", JSON.stringify(productCartArray));
+//    }
+// })
+
+
+//    function CartPage(cartItems) {
+//       cartList.style.display = "block";
+//       cartList.style.color = "red";
+//       cartList.innerHTML= "";
+//       cartItems.forEach(function(cartElement) {
+//         cartList.innerHTML += `
+//                               <div class="classlist">
+//                              <span>${cartElement.id}<span>
+//                              <span>${cartElement.name}<span>
+//                              </div>  
+//                              `
       
-   })
+//    })
 
   
-}
+// }
 
 
--- js/product.js
+++ js/product.js
@@ -1,133 +1,39 @@
 // const url = "https://www.rkamdahl.no/wp-json/wc/v3/products?consumer_key=ck_946e569b72881be9b01c9ebcbfb9761e36118143&consumer_secret=cs_cd563c29f6ce7fc749515dfe3912ec618804f8b6";
 
 
- const url = "https://www.rkamdahl.no/wp-json/wc/store/products?per_page=20";
+ const apIUrl = "https://www.rkamdahl.no/wp-json/wc/store/products ";
+ const itemsWrapper= document.querySelector(".men-section");
 
 async function getProduct(url){
-  try{
   const response = await fetch(url);
   const results =  await response.json();
   console.log(results);
-
-  }
-  catch (error){
-console.log("an error");
-  }
+  results.forEach(function(product){
+   itemsWrapper.innerHTML  +=
+                                `  
+                                    <div class ="cards">
+                                      <div class="products">
+                                      <a href="/products/products.html">
+                                      <img src="${product.images}" alt="" class="products"></a> 
+                                      </div>
+                                      <div class="price-1">
+                                      <p class="productName">${product.name}</p>
+                                      <p class="cost">${product.prices}</p>
+                                      </div>
+                                      <div class="add-to-cart-btn">
+                                      <button class="btn-small" data-product = ${product.id}>Add to cart</button>
+                                      </div>
+                                    </div> 
+                                      
+                              `
+  })
 
 }
-getProduct(url);
-/*
-  export const productsArray = [
+getProduct(apIUrl);
 
-  {
-  name: "Rainy, blue",
-  description: "rainy day jacket water proof",
-  image: "./images/hiking-2.png",
-  id: "1",
-  price: "950kr,-",
-  Total: "Total",
-  },
 
-  {
-    name: "Viking, green",
-    description: "rainy day jacket water proof",
-    image: "./images/manacketgreen.png",
-    id: "3",
-    price: "150kr,-",
-    Total: "Total",
-  }, 
 
-  {
-    name: "Hiking, purple",
-    description: "rainy day hiking  jacket water proof purple  ",
-    image: "./images/hiking-1.png",
-    id: "4",
-    price: "190kr,-",
-    Total: "Total",
-    
-  },
 
-  {
-    name: "Rainy, pink",
-    description: "rainy day pink jacket water proof pink unisex in new look",
-    image: "/images/manjacketpink.png",
-    id: "5",
-    price: "190kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Rainy, pink",
-    description: "rainy day pink jacket water proof unisex new style ",
-    image: "/images/manjacketpink.png",
-    id: "6",
-    price: "150kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Rainy, navy blue",
-    description: "rainy day water proof hiking jacket ",
-    image: "/images/hiking.png",
-    id: "7",
-    price: "195kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Hiking, purple",
-    description: "rainy day green jacket water proof hiking jacket purple ",
-    image: "/images/hiking-1.png",
-    id: "8",
-    price: "150kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Viking, green ",
-    description: "rainy day jacket water proof jacket unisex blue",
-    image: "images/womenunisexbluelight.png",
-    id: "9",
-    price: "190kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Viking green",
-    description: "rainy day green jacket water proof hiking jacket purple ",
-    image: "/images/hiking-1.png",
-    id: "10",
-    price: "50kr,-",
-    Total: "Total",
-  },
-
-  {
-    name: "Viking jacket",
-    description: "rainy day green jacket water proof hiking jacket ",
-    image: "/images/manacketgreen.png",
-    id: "11",
-    price: "180kr,-",
-    Total: "Total",
-    
-  },
-
-  {
-    name: "Viking pink",
-    description: "rainy day pink jacket water proof  ",
-    image: "/images/manjacketpink.png",
-    id: "12",
-    price: "1950kr,-",
-    Total: "Total",
-    
-  },
-
-]
-*/
 
 
 
