32c2f6db2823fca620e72ea1637335812076df93
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-08-24 13:44:28 +0200

    update

--- js/detail.js
+++ js/detail.js
@@ -4,26 +4,42 @@ const params = new URLSearchParams(queryString);
 console.log(params);
 const id = params.get("id");
 console.log(id);
-const linkUrl = "https://www.rkamdahl.no/wp-json/wc/store/products/? +id";
+const linkUrl = `https://www.rkamdahl.no/wp-json/wc/store/products/${id}`;
 
 console.log(linkUrl);
-
-async function fetchID(){
+async function fetchProductsID(){
   const response = await fetch(linkUrl);
-  const link = await response.json();
-  console.log(link);
+  const productsJacket = await response.json();
+  console.log(productsJacket);
 
-  linkCards.innerHTML +=  ` <div>
-                            <img src="" alt="" class="items-1">
-                            <h1>${link.id}</h1>
-                            <h2>${link.name}</h2>
-                            <p class="cost">${link.price}</p>
-                          </div>    
-                        `
-
+  document.querySelector(".items-1").innerHTML = productsJacket.images[0].src;
+  document.querySelector("h1").innerText = productsJacket.name;
+  document.querySelector(".sub-heading").innerHTML = productsJacket.short_description;
+  document.getElementById("price-2").innerHTML = productsJacket.prices.price;
+  document.querySelector(".details").innerHTML= productsJacket.description;
+  //document.querySelector(".cost").innerText= price;
+  document.querySelector(".in-stock").innerText= productsJacket.is_in_stock;
+      
 }
 
-fetchID()
+fetchProductsID()
+// async function fetchID(){
+//   const response = await fetch(linkUrl);
+//   const link = await response.json();
+//   console.log(link);
+
+//   linkCards.innerHTML +=  ` <div>
+//                             <img src="${link.images[0].src}" alt="" class="items-1">
+//                             <h1>${link.id}</h1>
+//                             <h2>${link.name}</h2>
+//                             <p class="cost">${link.prices.price}</p>
+//                             <p class="cost">${link.description}</p>
+//                           </div>    
+//                         `
+
+// }
+
+// fetchID()
 // const cartlist = JSON.parse(localStorage.getItem(".cartList"))
 // console.log(cartlist);
 
--- js/product.js
+++ js/product.js
@@ -8,13 +8,12 @@ async function getProduct(url){
   results.forEach(function(product){
    products.innerHTML  +=    ` <div class ="cards">
                                 <div class="products">
-                                <a href="/products/products.html?id=">
-                                <img src="${product.images[0].src}" alt="" class="products"> 
+                                <a href="/products/products.html?id=${product.id}">
+                                <img src="${product.images[0].src}" alt="product pictures" class="products"> 
                                 </div>
                                 <div class="price-1">
                                 <p class="productName">${product.name}</p>
                                 <p class="cost">${product.prices.price}</p>
-                                <p class="cost">${product.id}</p>
                                 </div></a>
                                 <div class="add-to-cart-btn">
                                 <button class="btn-small" data-product = ${product.id}>Add to cart</button>
